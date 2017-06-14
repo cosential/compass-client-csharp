59e2e751199b0bf7f13bfcefc264630fb6ff031d
Damon Warren <dwarren@cosential.com>
2017-06-14 12:14:31 -0500

    Added support for opportunity companies with role

---
+++ Models/CompanyIdent.cs
@@ -0,0 +1,8 @@
+﻿namespace Cosential.Integrations.Compass.Client.Models
+{
+    public class CompanyIdent
+    {
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Models/OpportunityCompany.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosential.Integrations.Compass.Client.Models
+{
+    public class OpportunityCompany
+    {
+        public int Id { get; set; }
+        public string Comments { get; set; }
+        public CompanyIdent Company { get; set; }
+        public ProjectRoleType RoleType { get; set; }
+    }
+}
