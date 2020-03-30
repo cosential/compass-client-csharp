8e6dfcf3ba49117a6fdfd674a9cfd7301867e9ac
Matt Butler <mbutler@cosential.com>
2020-03-30 15:26:13 -0500

    ticket/CO-6778 Firm Org Office Not Associating in Cosential

--- Models/Office.cs
+++ Models/Office.cs
@@ -4,6 +4,8 @@ namespace Cosential.Integrations.Compass.Client.Models
 {
     public class Office
     {
+        public Nullable<int> CompassId { get; set; }
+
         public int? OfficeID { get; set; }
         public string OfficeName { get; set; }
         public bool DeleteIndicator { get; set; }
